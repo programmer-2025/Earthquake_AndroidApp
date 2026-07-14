d56a173785d7e3eeee81e0309d010f853c16551d
programmer-2025 <snowsansub011307@gmail.com>
2026-07-14 21:20:35 +0900

    ＜追加＞ フラグメントを追加しました

---
+++ app/src/main/java/com/example/earthquake_androidapp/fragment/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.earthquake_androidapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.earthquake_androidapp.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {}
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
